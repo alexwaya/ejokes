54686affbcf23d9bc8707f48f1049f7917387789
Alexander Waya Nguku <alex.waya@gmail.com>
2022-03-21 23:28:04 +0300

    configure views

--- jokes/views.py
+++ jokes/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+import pyjokes
 
-# Create your views here.
+#import jokes
+
+def index(request):
+    jokes = pyjokes.get_joke()
+    return render(request, 'index.html', {'jokes:jokes'})
